b405963396766235a72c9d5e862f83e366423899
xxgcxy <xxgcxy@iMac-2.local>
2016-04-16 17:01:48 +0800

    初始化项目

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by xxgcxy on 16/4/16.
+//  Copyright (c) 2016年 it. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by xxgcxy on 16/4/16.
+//  Copyright (c) 2016年 it. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
