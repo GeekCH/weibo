528bcdcabeabf338c81b272cca6a2d3792108977
chd <chd@qq.com>
2016-08-08 18:00:52 +0800

    person

---
+++ weibo/weibo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weibo
+//
+//  Created by 爱生活Geek on 16/8/8.
+//  Copyright © 2016年 it. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weibo/weibo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weibo
+//
+//  Created by 爱生活Geek on 16/8/8.
+//  Copyright © 2016年 it. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
